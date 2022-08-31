d65011a8f2b2a436317f3889a636c370214efa36
Varsity Software <jweaver@iemic.com>
2022-08-30 20:15:36 -0400

    Updated to work with Capacitor 4.x.x

---
+++ ios/Plugin/CapacitorSafeArea.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+@objc public class CapacitorSafeArea: NSObject {
+    @objc public func getSafeAreaInsets(_ value: String) -> String {
+        return value
+    }
+
+    @objc public func getWindow() -> UIWindow? {
+        if #available(iOS 13, *) {
+            return UIApplication.shared.windows.first { $0.isKeyWindow }
+        } else {
+            return UIApplication.shared.keyWindow
+        }
+    }
+
+    @objc public func getStatusBarHeight() -> CGFloat {
+        var statusBarHeight: CGFloat = 0
+        if #available(iOS 13.0, *) {
+            let scene = UIApplication.shared.connectedScenes.first
+            guard let windowScene = scene as? UIWindowScene else { return 0 }
+            guard let statusBarManager = windowScene.statusBarManager else { return 0 }
+            statusBarHeight = statusBarManager.statusBarFrame.height
+        } else {
+            statusBarHeight = UIApplication.shared.statusBarFrame.height
+        }
+        return statusBarHeight
+    }
+}
---
+++ ios/Plugin/CapacitorSafeAreaPlugin.swift
@@ -0,0 +1,51 @@
+import Foundation
+import Capacitor
+
+/**
+ * Please read the Capacitor iOS Plugin Development Guide
+ * here: https://capacitorjs.com/docs/plugins/ios
+ */
+@objc(CapacitorSafeAreaPlugin)
+public class CapacitorSafeAreaPlugin: CAPPlugin {
+    private let implementation = CapacitorSafeArea()
+    
+    @objc func getSafeAreaInsets(_ call: CAPPluginCall) {
+        var top: CGFloat = 0.00,
+            bottom: CGFloat = 0.00,
+            right: CGFloat = 0.00,
+            left: CGFloat = 0.00
+        if #available(iOS 11.0, *) {
+            DispatchQueue.main.async {
+                let window = self.implementation.getWindow()
+                top = window?.safeAreaInsets.top ?? 0.00
+                bottom = window?.safeAreaInsets.bottom ?? 0.00
+                left = window?.safeAreaInsets.left ?? 0.00
+                right = window?.safeAreaInsets.right ?? 0.00
+                call.resolve([
+                    "insets": [
+                        "top": top,
+                        "bottom": bottom,
+                        "right": right,
+                        "left": left,
+                    ],
+                ])
+            }
+        } else {
+            call.resolve([
+                "insets": [
+                    "top": top,
+                    "bottom": bottom,
+                    "right": right,
+                    "left": left,
+                ],
+            ])
+        }
+    }
+
+    @objc func getStatusBarHeight(_ call: CAPPluginCall) {
+        DispatchQueue.main.async {
+            let barHeight = self.implementation.getStatusBarHeight()
+            call.resolve(["statusBarHeight": barHeight])
+        }
+    }
+}
